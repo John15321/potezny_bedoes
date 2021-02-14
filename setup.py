eb282704a90f1450199d2818596525963d5c401b
John15321 <janbronicki@gmail.com>
2021-02-14 02:43:59 +0100

    ugabuga

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='potezny_bedoes',
+    version='0.1dev',
+    packages=[],
+    license='Twoja Stara',
+    long_description=open('README.md').read(),
+)
---
+++ tests/test_add.py
@@ -0,0 +1,4 @@
+from potezny_bedoes.additions import adding
+
+def test_adding():
+    assert adding(1, 1) == 2
